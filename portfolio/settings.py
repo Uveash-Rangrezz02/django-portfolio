97f9ec8525b445fc404714ce43b12200635f60a8
Uveash <128137762+Uveash@users.noreply.github.com>
2026-01-07 16:10:58 +0530

    Fix email setting using env variables

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,13 +80,19 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # EMAIL (SECURE)
+import os
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+
+EMAIL_HOST_USER = os.environ.get("rangrezzuveash@gmail.com")
+EMAIL_HOST_PASSWORD = os.environ.get("jdrhfeqifxfyorec")
+
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 CSRF_TRUSTED_ORIGINS = [
     "https://django-portfolio-iqcm.onrender.com"
